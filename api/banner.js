343857b8f6f08cab1af79ffdac5bb5366c238fda
mskim0344 <mskim0344@gmail.com>
2023-08-22 12:11:34 +0900

    404에러 페이지 생성과 로컬호스트 연결 확인 완료

---
+++ api/banner.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+//라우터
+router.get('/', (req, res)=>{
+  res.send('localhost:8000/ 라고 주소에 치셨군요asdfasdf')
+});
+
+router.get('/sub', (req, res)=>{
+    res.send('localhost:8000/sub 라고 주소에 치셨군요')
+});
+
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require('express')();
+const app = require('express')();
+// const navi =require("./api/navi");
+const banner =require("./api/banner");
+// const interview =require("./api/interview");
 
-// const express = require('express');  위에 식이랑 같은거임  두줄을 한줄로 줄였다
-// const app = express();
 
-application.listen(8000, ()=>{
+//라우터
+// app.use('/', interview);
+app.use('/', banner);
+// app.use('/navi', navi);
+
+app.use((req, res)=>{
+     res.status(404).sendFile(__dirname+"/public/nopage.html")
+})
+
+
+
+app.listen(8000, ()=>{
     console.log("서버구동완료")
 })
