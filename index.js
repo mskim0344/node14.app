c7a642b4fc7d38ce438645e241faa815deedcdb8
mskim0344 <mskim0344@gmail.com>
2023-08-22 10:50:01 +0900

    노드 리엑트 서버 연동

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')();
+
+// const express = require('express');  위에 식이랑 같은거임  두줄을 한줄로 줄였다
+// const app = express();
+
+application.listen(8000, ()=>{
+    console.log("서버구동완료")
+})
